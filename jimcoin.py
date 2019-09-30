fdd510c9dfd4c127ee74e34d34fafe4f38da618a
jimmy <jim@thinkpad.gr>
2019-09-30 21:06:09 +0300

    refactoring

--- jimcoin.py
+++ jimcoin.py
@@ -7,11 +7,11 @@ num_of_blocks_to_add = 4
 # Create the blockchain and add the genesis block
 # blockchain is an array of Block objects
 blockchain = [create_genesis_block()]
-previous_block = blockchain[0]
+last_block = blockchain[0]
 
 # Add blocks to the chain
 for i in range(0, num_of_blocks_to_add):
-    block_to_add = next_block(previous_block)
+    block_to_add = next_block(last_block)
     blockchain.append(block_to_add)
-    previous_block = block_to_add
-    print_block(block_to_add);
+    last_block = block_to_add
+    print_block(block_to_add)
